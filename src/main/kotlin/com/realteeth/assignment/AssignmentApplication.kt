5e8275730c6bbe924190046b47a535b629b5b7b6
ljm88 <jmjang88@naver.com>
2026-03-05 16:58:27 +0900

    [feat] 스케줄링 활성화

--- src/main/kotlin/com/realteeth/assignment/AssignmentApplication.kt
+++ src/main/kotlin/com/realteeth/assignment/AssignmentApplication.kt
@@ -2,7 +2,9 @@ package com.realteeth.assignment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class AssignmentApplication
 
