95603d9a90b5180aec0dafb5192ad6f5c30bf736
ljm88 <jmjang88@naver.com>
2026-03-06 15:13:28 +0900

    [feat] 리트라이, 비동기 처리 활성화

--- src/main/kotlin/com/realteeth/assignment/AssignmentApplication.kt
+++ src/main/kotlin/com/realteeth/assignment/AssignmentApplication.kt
@@ -2,9 +2,13 @@ package com.realteeth.assignment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.retry.annotation.EnableRetry
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
+@EnableAsync
+@EnableRetry
 @SpringBootApplication
 class AssignmentApplication
 
