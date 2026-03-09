4d368178fcefc170c6f8d52be62121d8ced635b9
jmjang88 <jmjang88@naver.com>
2026-03-09 20:38:57 +0900

    [feat] MockWorkerClient 상태 코드에 따른 분기 세분화

--- src/main/kotlin/com/realteeth/assignment/infrastructure/client/MockWorkerClient.kt
+++ src/main/kotlin/com/realteeth/assignment/infrastructure/client/MockWorkerClient.kt
@@ -32,12 +32,23 @@ class MockWorkerClient(
                 request
             )
 
-            if (response.statusCode.is2xxSuccessful && response.body != null) {
-                log.info("[ImageTask] behavior: 작업 요청 | SUCCESS | response: ${response.body} | message: Mock Worker 작업 요청")
-                Result.success(response.body!!)
-            } else {
-                log.error("[ImageTask] behavior: 작업 요청 | FAIL | status: ${response.statusCode} | message: Mock Worker 작업 요청 실패")
-                Result.failure(MockWorkerException("Mock Worker 서버 작업 요청에 실패했습니다.: ${response.statusCode}"))
+            when {
+                response.statusCode.is2xxSuccessful && response.body != null -> {
+                    log.info("[ImageTask] behavior: 작업 요청 | SUCCESS | response: ${response.body} | message: Mock Worker 작업 요청")
+                    Result.success(response.body!!)
+                }
+                response.statusCode.is2xxSuccessful && response.body == null -> {
+                    log.error("[ImageTask] behavior: 작업 요청 | FAIL | status: ${response.statusCode} | message: Mock Worker 응답 body null")
+                    Result.failure(MockWorkerException("Mock Worker 서버의 응답이 null입니다.: ${response.statusCode}"))
+                }
+                response.statusCode.is4xxClientError -> {
+                    log.error("[ImageTask] behavior: 작업 요청 | FAIL | status: ${response.statusCode} | message: Mock Worker 작업 요청 에러")
+                    Result.failure(MockWorkerException("Mock Worker 서버 작업 요청에 실패했습니다.: ${response.statusCode}"))
+                }
+                else -> {
+                    log.error("[ImageTask] behavior: 작업 요청 | FAIL | status: ${response.statusCode} | message: Mock Worker 서버 에러")
+                    Result.failure(MockWorkerException("Mock Worker 서버에서 오류가 발생했습니다.: ${response.statusCode}"))
+                }
             }
         } catch (e: RestClientException) {
             log.error("[ImageTask] behavior: 작업 요청 | FAIL | message: Mock Worker 통신 실패")
