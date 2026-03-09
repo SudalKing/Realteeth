d5f3be98f449646c8e09f02580304e4ea87fc4d1
ljm88 <jmjang88@naver.com>
2026-03-09 16:07:00 +0900

    [test] test 프로필 생성

--- src/test/kotlin/com/realteeth/assignment/AssignmentApplicationTests.kt
+++ src/test/kotlin/com/realteeth/assignment/AssignmentApplicationTests.kt
@@ -2,8 +2,10 @@ package com.realteeth.assignment
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AssignmentApplicationTests {
 
     @Test
